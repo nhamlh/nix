ab7ff4416a4603f8f90d7003dcfac9429bc01b5c
Nham Le <lehoainham@gmail.com>
2022-12-27 17:34:27 +0700

    amd-desktop: install tailscale + agenix

--- flake.nix
+++ flake.nix
@@ -8,13 +8,19 @@
       nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";  # for packages on the edge
       home-manager.url = "github:rycee/home-manager/master";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+      # Secrets manager
+      agenix.url = "github:ryantm/agenix";
+      agenix.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  outputs = inputs@{ self, home-manager, nixpkgs, ... }: {
+  outputs = inputs@{ self, home-manager, nixpkgs, agenix, ... }: {
     nixosConfigurations.amd-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = inputs;
       modules = [
         ./machines/amd-desktop
+        agenix.nixosModule
         home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
---
+++ machines/amd-desktop/secrets/secrets.nix
@@ -0,0 +1,6 @@
+let
+  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmPhGYUluyUQ2j/pcF+2hyC38HBMnkyPYd3Mq3IlI8d nhamlh@somewhereonearth";
+in
+{
+  "tskey.age".publicKeys = [ key ];
+}
