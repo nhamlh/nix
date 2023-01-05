59f44603ed3817b8b1578024b2c6cb29d4b30f82
Nham Le <lehoainham@gmail.com>
2023-01-05 12:03:07 +0700

    wip

--- flake.nix
+++ flake.nix
@@ -1,32 +1,33 @@
 {
   description = "My journey with Nix";
 
-  inputs = 
-    {
-      # Core dependencies.
-      nixpkgs.url = "nixpkgs/nixos-unstable";             # primary nixpkgs
-      nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";  # for packages on the edge
-      home-manager.url = "github:rycee/home-manager/master";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    # Core dependencies.
+    nixpkgs.url = "nixpkgs/nixos-unstable"; # primary nixpkgs
+    nixpkgs-unstable.url =
+      "nixpkgs/nixpkgs-unstable"; # for packages on the edge
+    home-manager.url = "github:rycee/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-      # Secrets manager
-      agenix.url = "github:ryantm/agenix";
-      agenix.inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Secrets manager
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = inputs@{ self, home-manager, nixpkgs, agenix, ... }: {
     nixosConfigurations.amd-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = inputs;
       modules = [
+        ./modules
         ./machines/amd-desktop
         agenix.nixosModule
         home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.nhamlh = import ./home;
-          }
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nhamlh = import ./home;
+        }
       ];
     };
   };
