c2779e3d649ad246b3e1ea21c6504c24be504fbc
Nham Le <lehoainham@gmail.com>
2023-01-06 10:24:06 +0700

    feat: add services.grafana-agent module

--- modules/base/agenix.nix
+++ modules/base/agenix.nix
@@ -17,6 +17,9 @@ in {
     age = {
       identityPaths = [ "${config.users.users.nhamlh.home}/.ssh/id_ed25519" ];
       secrets.tskey.file = ../../secrets/tskey.age;
+      # Grafana Agent API keys
+      secrets.ga-prom-key.file = ../../secrets/ga-prom-key.age;
+      secrets.ga-loki-key.file = ../../secrets/ga-loki-key.age;
     };
   };
 }
