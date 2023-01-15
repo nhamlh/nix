8be362e4e9ed8b3052b23a9c546b305f8aeec0c0
Nham Le <lehoainham@gmail.com>
2023-01-15 11:15:12 +0700

    amd-desktop/fs: use by-uuid temporarily
    I'm having 2 disks with the same set of labels right now so it's
    clashing together. Use by-uuid as a test to avoid label clashing.

--- hosts/amd-desktop/hardware.nix
+++ hosts/amd-desktop/hardware.nix
@@ -17,16 +17,17 @@
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
+    device = "/dev/disk/by-uuid/76e0ff94-4634-422f-9e81-7bb19d9fd253";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
+    device = "/dev/disk/by-uuid/6C8A-108A";
     fsType = "vfat";
   };
 
-  swapDevices = [ ];
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/6b163ea2-fda4-40ee-b20e-aee14f889234"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
