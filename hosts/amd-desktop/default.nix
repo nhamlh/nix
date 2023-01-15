c05551e4bfe76c9b6d121a617e540d283fd47b5c
Nham Hoai Le <lehoainham@gmail.com>
2023-01-15 12:55:16 +0700

    move user settings to modules
    so that every hosts will have the same users settings

--- hosts/amd-desktop/default.nix
+++ hosts/amd-desktop/default.nix
@@ -22,17 +22,5 @@
     gaming = { steam.enable = true; };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nhamlh = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
-    home = "/home/nhamlh";
-    #NOTE: This password (created by mkpasswd -m sha-512) is for seed purpose. Must be updated after installation.
-    hashedPassword =
-      "$6$aQoR2srY/MEt/QsN$US5maz2FoHLqMKVAWjsGVOpEsNXN4HgYEqexlkiGb59XOc/HqTxd8p0UDzVERmISGuYd0Y6hj4Z9pfDlzT3150";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmPhGYUluyUQ2j/pcF+2hyC38HBMnkyPYd3Mq3IlI8d nhamlh@somewhereonearth"
-    ];
-  };
   system.stateVersion = "22.05"; # Did you read the comment?
 }
