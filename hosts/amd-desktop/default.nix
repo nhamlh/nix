f45d3b819b42e7561a3408871117c9f4681d804f
Nham Le <lehoainham@gmail.com>
2023-01-15 11:06:10 +0700

    seed user password
    need to change after installation

--- hosts/amd-desktop/default.nix
+++ hosts/amd-desktop/default.nix
@@ -33,6 +33,9 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
     home = "/home/nhamlh";
+    #NOTE: This password (created by mkpasswd -m sha-512) is for seed purpose. Must be updated after installation.
+    hashedPassword =
+      "$6$aQoR2srY/MEt/QsN$US5maz2FoHLqMKVAWjsGVOpEsNXN4HgYEqexlkiGb59XOc/HqTxd8p0UDzVERmISGuYd0Y6hj4Z9pfDlzT3150";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmPhGYUluyUQ2j/pcF+2hyC38HBMnkyPYd3Mq3IlI8d nhamlh@somewhereonearth"
     ];
