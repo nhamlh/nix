54c9f56d8d0b1765856ea86baa81f4f97fd23578
Nham Hoai Le <lehoainham@gmail.com>
2023-03-26 16:26:18 +0700

    fix user password

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,7 +3,12 @@
 with builtins;
 with lib;
 
-let hm = home-manager.nixosModules.home-manager;
+let
+  hm = home-manager.nixosModules.home-manager;
+  secrets = import config.age.secrets.secrets.path;
+  #NOTE: This password (created by mkpasswd -m sha-512) is for seed purpose. Must be updated after installation.
+  defaultHashedPasswd =
+    "$6$YrkWwzXEm7Aw11ie$gxRMYpvbdx6m07AGKsZxMCVcGN1br2Pm1y.yikHxxbIG0NsbTRhRNIZF42qmRK/GGPDVwxEHepcYMCwgc1QfW.";
 in {
   imports = [ hm { } ./base ./graphical ];
 
@@ -12,9 +17,10 @@ in {
       isNormalUser = true;
       extraGroups = [ "wheel" "docker" ];
       home = "/home/nhamlh";
-      #NOTE: This password (created by mkpasswd -m sha-512) is for seed purpose. Must be updated after installation.
-      #hashedPassword = removeSuffix "\n" (readFile config.age.secrets.nhamlh-passwd.path);
-      hashedPassword = "$6$YrkWwzXEm7Aw11ie$gxRMYpvbdx6m07AGKsZxMCVcGN1br2Pm1y.yikHxxbIG0NsbTRhRNIZF42qmRK/GGPDVwxEHepcYMCwgc1QfW.";
+      hashedPassword = if secrets.nhamlh-passwd != "" then
+        secrets.nhamlh-passwd
+      else
+        defaultHashedPasswd;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmPhGYUluyUQ2j/pcF+2hyC38HBMnkyPYd3Mq3IlI8d nhamlh@somewhereonearth"
       ];
@@ -32,4 +38,3 @@ in {
     };
   };
 }
-
