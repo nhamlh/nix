9cc9b897a9cf6757a0815b36ad49cb94564806ac
Nham Le <lehoainham@gmail.com>
2023-07-09 10:16:15 +0700

    doomemacs/python: enable tree-sitter and pyright integration

--- modules/home/base/doom/init.el
+++ modules/home/base/doom/init.el
@@ -107,7 +107,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        terraform           ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -163,7 +163,10 @@
        php                 ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp)       ; beautiful is better than ugly
+       (python             ; beautiful is better than ugly
+        +lsp
+        +tree-sitter
+        +pyright)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
