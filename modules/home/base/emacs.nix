a5ad5bb050845bbc7151e1b2bd9ef8057f994057
Nham Le <lehoainham@gmail.com>
2023-08-03 09:23:03 +0700

    emacs29.1

--- modules/home/base/emacs.nix
+++ modules/home/base/emacs.nix
@@ -3,9 +3,10 @@
 {
   config = {
     home-manager.users.nhamlh = {
-    #TODO: setup doomemacs properly according to https://github.com/doomemacs/doomemacs/blob/master/docs/getting_started.org#doom-emacs
+      #TODO: setup doomemacs properly according to https://github.com/doomemacs/doomemacs/blob/master/docs/getting_started.org#doom-emacs
       programs.emacs = {
         enable = true;
+        package = pkgs.emacs29;
         extraPackages = epkgs: [ epkgs.vterm ];
       };
 
