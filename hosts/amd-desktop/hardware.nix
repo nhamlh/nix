c26a1126b4f7b107b862a05535826a9e1139bf20
Nham Le <lehoainham@gmail.com>
2023-10-20 13:40:43 +0700

    amd-desktop to update swap device id

--- hosts/amd-desktop/hardware.nix
+++ hosts/amd-desktop/hardware.nix
@@ -27,7 +27,7 @@
   };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/6b163ea2-fda4-40ee-b20e-aee14f889234"; }];
+    [{ device = "/dev/disk/by-uuid/d6bd177c-8020-4de6-8db8-9d9ca09f7858"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
