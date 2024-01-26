ecc37b6df12c3ca3536fb728c411d6b65cbf8788
Nham Le <lehoainham@gmail.com>
2024-01-26 22:03:51 +0700

    fix alacritty issue with cursor

--- modules/home/graphical/alacritty/default.nix
+++ modules/home/graphical/alacritty/default.nix
@@ -7,6 +7,17 @@ in {
     home-manager.users.nhamlh = {
       home.packages = with pkgs; [ alacritty ];
 
+      # Fix hidden cursor after typing and need to move the cursor out of Alacritty windows for it to be visible again.
+      home.pointerCursor = {
+        name = "Adwaita";
+        package = pkgs.gnome.adwaita-icon-theme;
+        size = 24;
+        x11 = {
+          enable = true;
+          defaultCursor = "Adwaita";
+        };
+      };
+
       xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
     };
   };
