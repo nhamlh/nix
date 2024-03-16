354cb9cf56718838226230539c98a02239c0f47d
Nham Le <lehoainham@gmail.com>
2024-03-16 11:01:57 +0700

    doom: stop pinning magit deps version
    they're working fine now.

--- modules/home/base/doom/packages.el
+++ modules/home/base/doom/packages.el
@@ -16,15 +16,3 @@
 
 ;; doom uses a little old format-all package, unpit to use up-to-date version
 ;;(unpin! format-all)
-
-;; It seems that some magit dependencies break with Emacs 28, as the function
-;; defvar-keymap is only added in Emacs 29. The solution is to pin an older
-;; version of the packages, as Doom currently does not support Emacs 29.
-;; https://emacs.stackexchange.com/questions/75827/doom-emacs-error-running-hook-global-git-commit-mode-because-void-variable
-(package! transient
-      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
-      :recipe (:host github :repo "magit/transient"))
-
-(package! with-editor
-          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
-          :recipe (:host github :repo "magit/with-editor"))
