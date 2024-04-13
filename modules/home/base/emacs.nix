2488c65cf562a306c0c4a351b7ee1ebc3a33c80c
Nham Le <lehoainham@gmail.com>
2024-04-13 15:09:51 +0700

    emacs: install tree-sitter and grammars

--- modules/home/base/emacs.nix
+++ modules/home/base/emacs.nix
@@ -2,6 +2,33 @@
 
 {
   config = {
+    environment.systemPackages = with pkgs; [
+      tree-sitter
+      # Got errors installing grammars with home-manager thus installing at system level
+      tree-sitter-grammars.tree-sitter-yaml
+      tree-sitter-grammars.tree-sitter-ruby
+      tree-sitter-grammars.tree-sitter-python
+      tree-sitter-grammars.tree-sitter-go
+      tree-sitter-grammars.tree-sitter-gomod
+      tree-sitter-grammars.tree-sitter-elisp
+      tree-sitter-grammars.tree-sitter-dockerfile
+      tree-sitter-grammars.tree-sitter-cmake
+      tree-sitter-grammars.tree-sitter-bash
+      tree-sitter-grammars.tree-sitter-sql
+      tree-sitter-grammars.tree-sitter-toml
+      tree-sitter-grammars.tree-sitter-regex
+      tree-sitter-grammars.tree-sitter-nix
+      tree-sitter-grammars.tree-sitter-markdown
+      tree-sitter-grammars.tree-sitter-lua
+      tree-sitter-grammars.tree-sitter-latex
+      tree-sitter-grammars.tree-sitter-json
+      tree-sitter-grammars.tree-sitter-json5
+      tree-sitter-grammars.tree-sitter-javascript
+      tree-sitter-grammars.tree-sitter-html
+      tree-sitter-grammars.tree-sitter-hcl
+
+    ];
+
     home-manager.users.nhamlh = {
       #TODO: setup doomemacs properly according to https://github.com/doomemacs/doomemacs/blob/master/docs/getting_started.org#doom-emacs
       programs.emacs = {
