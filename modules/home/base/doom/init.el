349676485236e6a51e886c23eae0e5d051a07de2
Nham Le <lehoainham@gmail.com>
2024-08-31 13:53:15 +0700

    doom: disable docker
    it's messing with my Dockerfile: auto-reformat to invalid
    Dockerfile (tab instead of space)

--- modules/home/base/doom/init.el
+++ modules/home/base/doom/init.el
@@ -92,7 +92,7 @@
        debugger            ; FIXME stepping through code, to help you add bugs
        ;;biblio            ; Writes a PhD for you (citation needed)
        direnv
-       docker
+       ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
