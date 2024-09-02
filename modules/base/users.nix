9c9f279cb6a3387270b58b49292f85f82e31d404
Nham Le <lehoainham@gmail.com>
2024-09-02 17:22:52 +0700

    merge home to base

--- modules/base/users.nix
+++ modules/base/users.nix
@@ -1,6 +1,10 @@
-{ options, config, lib, pkgs, ... }:
+{ config, lib, pkgs, home-manager, ... }:
 
-with lib; {
+let
+  hm = home-manager.nixosModules.home-manager;
+  secrets = config.age.secrets.secrets.path;
+  user = "nhamlh";
+in with lib; {
   config = {
     users = {
       # Ref: https://nixos.org/manual/nixos/stable/options.html#opt-users.mutableUsers
@@ -10,6 +14,28 @@ with lib; {
       # as a flake can be considered secure enough
       mutableUsers = false;
       defaultUserShell = pkgs.zsh;
+
+      users.nhamlh = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" "docker" ];
+        home = "/home/${user}";
+        hashedPasswordFile =
+          "${config.age.secrets.secrets.path}_${user}-passwd";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmPhGYUluyUQ2j/pcF+2hyC38HBMnkyPYd3Mq3IlI8d nhamlh@somewhereonearth"
+        ];
+      };
+    };
+
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+
+    home-manager.users.nhamlh = {
+      home.username = user;
+      home.homeDirectory = "/home/${user}";
+
+      home.stateVersion = "22.05";
+      programs.home-manager.enable = true;
     };
   };
 }
