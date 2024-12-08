549b999ced2f28996747aafc6944a13c9635fc0e
Nham Le <lehoainham@gmail.com>
2024-12-08 20:13:37 +0700

    switch system to stable nixpkgs channel

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Core dependencies.
-    nixpkgs.url = "nixpkgs/nixos-unstable"; # primary nixpkgs
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     # nixpkgs-unstable.url =
     #   "nixpkgs/nixpkgs-unstable"; # for packages on the edge
     home-manager.url = "github:rycee/home-manager/master";
@@ -17,35 +17,26 @@
     };
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
         inherit system;
-        config = {
-          allowUnfree = true;
-        };
+        config = { allowUnfree = true; };
       };
 
-      hosts = pkgs.lib.mapAttrsToList (n: v: n) (
-        pkgs.lib.filterAttrs (n: v: v == "directory") (builtins.readDir ./hosts)
-      );
-    in
-    {
-      nixosConfigurations = pkgs.lib.genAttrs hosts (
-        h:
+      hosts = pkgs.lib.mapAttrsToList (n: v: n)
+        (pkgs.lib.filterAttrs (n: v: v == "directory")
+          (builtins.readDir ./hosts));
+    in {
+      nixosConfigurations = pkgs.lib.genAttrs hosts (h:
         nixpkgs.lib.nixosSystem {
           inherit system pkgs;
 
           specialArgs = inputs;
-          modules = [
-            ./modules
-            (./. + "/hosts/${h}")
-          ];
-        }
-      );
+          modules = [ ./modules (./. + "/hosts/${h}") ];
+        });
 
       devShells = import ./shell.nix { inherit system pkgs; };
     };
