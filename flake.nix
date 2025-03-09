65068cb4f2ab51b9d6fb56d97afa46b92c440fc8
Nham Le <lehoainham@gmail.com>
2025-03-09 11:14:19 +0700

    nix flake update and update home-manager location

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     nixpkgs.url = "nixpkgs/nixos-24.11";
     nixpkgs-unstable.url =
       "nixpkgs/nixpkgs-unstable"; # for packages on the edge
-    home-manager.url = "github:rycee/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Secrets manager
     agenix.url = "github:ryantm/agenix";
