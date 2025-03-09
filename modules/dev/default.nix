855e51efec86addde70e324128d943d52814fb7f
Nham Le <lehoainham@gmail.com>
2025-03-09 12:17:22 +0700

    fix vscode server can't install extentions

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -13,6 +13,16 @@ in {
 
   config = mkIf cfg.enable {
     home-manager.users.nhamlh = {
+
+      # For remote-editing vscode
+      imports = [
+        "${
+          fetchTarball
+          "https://github.com/msteen/nixos-vscode-server/tarball/master"
+        }/modules/vscode-server/home.nix"
+      ];
+      services.vscode-server.enable = true;
+
       home.packages = with pkgs-unstable; [
         aider-chat
         aichat
