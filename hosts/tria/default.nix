6ac0d682f682db25553723892982bca3b59f748b
ntvcom <nham@tevi.com>
2026-08-07 14:26:01 +0700

    tria: add ZFS, enable containers, disable k3s

--- hosts/tria/default.nix
+++ hosts/tria/default.nix
@@ -12,11 +12,24 @@
   time.timeZone = "Asia/Ho_Chi_Minh";
 
   my.modules = {
-    containers.k3s = {
+    containers = {
       enable = true;
-      role = "agent";
-      masterAddr = "https://ena:6443";
+      k3s = {
+        enable = false;
+        role = "agent";
+        masterAddr = "https://ena:6443";
+      };
     };
+  };
+
+  # ZFS support.
+  # Ref: https://openzfs.github.io/openzfs-docs/Getting%20Started/NixOS/index.html
+  # Ref: https://nixos.wiki/wiki/ZFS
+  networking.hostId = "677dda4e"; # head -c4 /dev/urandom | od -A none -t x4
+  boot = {
+    supportedFilesystems = [ "zfs" ];
+    zfs.forceImportRoot = false;
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   };
 
   system.stateVersion = "24.05";
